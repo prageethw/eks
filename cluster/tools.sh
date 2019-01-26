fbb0022e7a8f8a643d3d254839dbfe6312683c8e
Prageeth Warnak <prageeth@yahoo.co.uk>
2019-01-26 18:53:03 +1100

    fix pdb

--- cluster/tools.sh
+++ cluster/tools.sh
@@ -70,10 +70,10 @@ helm install stable/metrics-server \
     --version 2.0.4 \
     --set replicas=2 \
     --namespace metrics \
-    --set args={"--kubelet-insecure-tls=true"} \
-    --set resources.limits.cpu="200m",resources.limits.memory="100Mi"
+    --set resources.limits.cpu="100m",resources.limits.memory="50Mi"
 kubectl -n metrics rollout status deployment metrics-server
 kubectl apply -f resources/metrics-server-hpa.yaml
+kubectl apply -f resources/metrics-server-pdb.yaml
 
 # install monitoring and alerting tools
 # enable basic auth
@@ -95,6 +95,7 @@ helm install stable/prometheus \
     --set alertmanager.resources.requests.cpu="100m",server.resources.requests.memory="0.5Gi" \
     --set alertmanager.statefulSet.enabled="true" \
     -f resources/monitoring-alerting-limits.yml
+kubectl -n metrics rollout status statefulset prometheus-alertmanager
 kubectl -n metrics rollout status statefulset prometheus-server
 kubectl apply -f resources/prometheus-pdb.yaml
 
