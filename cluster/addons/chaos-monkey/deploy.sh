53e4c6138b075621dc961a0881eebe60aecca0ee
Prageeth Warnak <prageeth.warnak@team.telstra.com>
2019-02-01 20:11:55 +1100

    updated chaos monkey stuff
    Signed-off-by: Prageeth Warnak <prageeth.warnak@team.telstra.com>

--- cluster/addons/chaos-monkey/deploy.sh
+++ cluster/addons/chaos-monkey/deploy.sh
@@ -1,2 +1,6 @@
-echo "wait till chaos monkey deployed as defined in Chaosfile.json "
-chaos-lambda deploy -c Chaosfile.json
+source ../../k8s-eks-cluster.temp
+IFS=, read ng1 ng2 ng3 <<< "$ASG_NAMES"
+ASG_NAMES="\"$ng1\"","\"$ng2\"","\"$ng3"\"
+cat Chaosfile.json  | sed -e  "s@\"NODE_ASGS\"@$ASG_NAMES@g" | tee chaos_file.temp.json
+echo "wait till chaos monkey deployed ... "
+chaos-lambda deploy -c chaos_file.temp.json
